c7120f1fa9c7150f015ee3609753d9e5e401bfe3
Edisonwilliam <admiralcavani@gmail.com>
2026-09-04 12:52:07 +0100

    second commit

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from "express"
 import "dotenv/config"
 import cors from "cors"
 import dns from "node:dns"
-import { clerkMiddleware } from '@clerk/express'
+import { clerkMiddleware } from "@clerk/express"
 
 import connectDB from "./configs/db.js"
 import ClerkWebhooks from "./controllers/clerkWebhooks.js"
@@ -10,23 +10,19 @@ import ClerkWebhooks from "./controllers/clerkWebhooks.js"
 dns.setServers(["8.8.8.8", "8.8.4.4", "1.1.1.1"])
 
 const app = express()
-app.use(cors())
 
-//Middleware
+app.use(cors())
 app.use(express.json())
 app.use(clerkMiddleware())
 
-//api to listen to clerk webhooks
+// Connect to MongoDB
+connectDB()
+
+// Clerk webhook
 app.use("/api/clerk", ClerkWebhooks)
 
 app.get("/", (req, res) => {
   res.send("API is working")
 })
 
-const PORT = process.env.PORT || 3000
-
-app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`)
-})
-
-connectDB()
+export default app
